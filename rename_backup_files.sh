6b79ee47a083679de1f368e2a48bdd8ea3886720
azemoning <ahmadsaufimaulana@gmail.com>
2019-05-15 09:44:22 +0700

    remove unused file

--- rename_backup_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for sql in *.sql.gz; do
-        mv "$sql" "$(basename "$sql" .sql.gz).sql.gz.old"
-done
-for dump in *.dump; do
-        mv "$dump" "$(basename "$dump" .dump).dump.old"
-done
