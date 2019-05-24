e4a7ebefcfaad3f7dbafaa45e40c25f0bf784a3f
azemoning <ahmadsaufimaulana@gmail.com>
2019-05-24 08:34:27 +0700

    add ping before deleting old backup files

--- delete_backup.sh
+++ delete_backup.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+## PING HEALTHCHECKS BEFORE DELETING OLD BACKUP FILES
+curl -fsS --retry 3 https://hc-ping.com/6ca8dfa3-b052-476e-b594-d549521a79e4 > /dev/null
+
+## START DELETING OLD BACKUP FILES
 find . -name "*.old" -type f -delete
