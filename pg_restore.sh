fe00ef931a4ae35c1e93c10b55f2551d6b83599a
azemoning <ahmadsaufimaulana@gmail.com>
2019-05-24 11:13:49 +0700

    change format text to unix

--- pg_restore.sh
+++ pg_restore.sh
@@ -2,12 +2,12 @@
 
 ## SET ENVIRONMENT VARIABLES
 export PGSSLMODE="allow"
-export PGPASSWORD="password"
-export PGDB_HOST="host"
-export PGDB_PORT="5432"
-export PGDB_USER="user"
-export PGDB_DATABASE="db"
-export BACKUP_FILE_NAME="backup.dump"
+export PGPASSWORD="test.databas3"
+export PGDB_HOST="40.90.188.178"
+export PGDB_PORT="5000"
+export PGDB_USER="telinmy.testing"
+export PGDB_DATABASE="payment_gateway_testing"
+export BACKUP_FILE_NAME="payment_gateway_testing.dump"
 
 ## START RESTORE DATABASE
 pg_restore -v --no-owner --host=$PGDB_HOST --port=$PGDB_PORT --username=$PGDB_USER --dbname=$PGDB_DATABASE $BACKUP_FILE_NAME
