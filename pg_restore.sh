014411fbf2da5acf36b065b62aaf463658d93352
azemoning <ahmadsaufimaulana@gmail.com>
2019-07-31 10:07:14 +0700

    change environment variables value

--- pg_restore.sh
+++ pg_restore.sh
@@ -2,12 +2,12 @@
 
 ## SET ENVIRONMENT VARIABLES
 export PGSSLMODE="allow"
-export PGPASSWORD="test.databas3"
-export PGDB_HOST="40.90.188.178"
-export PGDB_PORT="5000"
-export PGDB_USER="telinmy.testing"
-export PGDB_DATABASE="payment_gateway_testing"
-export BACKUP_FILE_NAME="payment_gateway_testing.dump"
+export PGPASSWORD="password"
+export PGDB_HOST="host"
+export PGDB_PORT="5432"
+export PGDB_USER="username"
+export PGDB_DATABASE="database"
+export BACKUP_FILE_NAME="backup-file-name"
 
 ## START RESTORE DATABASE
 pg_restore -v --no-owner --host=$PGDB_HOST --port=$PGDB_PORT --username=$PGDB_USER --dbname=$PGDB_DATABASE $BACKUP_FILE_NAME
