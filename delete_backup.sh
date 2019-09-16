b8a271a5595a05dfaef205f40e37cfa35f7e250d
azemoning <ahmadsaufimaulana@gmail.com>
2019-09-16 14:39:09 +0700

    change to absolute path for deleting *.dump file

--- delete_backup.sh
+++ delete_backup.sh
@@ -4,4 +4,4 @@
 curl -fsS --retry 3 https://hc-ping.com/6ca8dfa3-b052-476e-b594-d549521a79e4 > /dev/null
 
 ## START DELETING OLD BACKUP FILES
-find . -name "*.old" -type f -delete
+find /root/pg_backup/backup_dir -name "*.dump" -type f -delete
