9bc791fb6c5607e413a4fb6cad658e8c3fbb595a
azemoning <ahmadsaufimaulana@gmail.com>
2019-10-23 10:20:46 +0700

    add info output

--- delete_backup.sh
+++ delete_backup.sh
@@ -3,5 +3,11 @@
 ## PING HEALTHCHECKS BEFORE DELETING OLD BACKUP FILES
 curl -fsS --retry 3 https://hc-ping.com/6ca8dfa3-b052-476e-b594-d549521a79e4 > /dev/null
 
+echo -e " "
+echo -e "Deleting local old backup files..."
+
 ## START DELETING OLD BACKUP FILES
 find /root/pg_backup/backup_dir -name "*.dump" -type f -delete
+
+echo -e "Delete complete."
+echo -e " "
